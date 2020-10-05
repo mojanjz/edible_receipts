f1b71dee0a79862851ac8659cf175198a217b457
Mojan Jamalzadeh <mojanjamalzadeh@yahoo.com>
2020-10-05 14:16:02 -0700

    done with implementation, needs comments

--- kern/synchprobs/airballoon.c
+++ kern/synchprobs/airballoon.c
@@ -7,7 +7,7 @@
 #include <test.h>
 #include <synch.h>
 
-#define N_LORD_FLOWERKILLER 2
+#define N_LORD_FLOWERKILLER 8
 #define NROPES 16
 static volatile int ropes_left = NROPES;
 
@@ -19,9 +19,9 @@ struct rope {
 	int rp_number;
 };
 
-/* Initialize array of ropes  and array of pointers to the ropes, stakes */
-struct rope ropes[NROPES];
-/* Volatile since stakes are accessed by all FlowerKillers */
+/* Initialize array of ropes, hooks, and array of pointers to the ropes, stakes */
+struct rope *ropes[NROPES];
+/* volatile since multiple flowekiller threads can access the stakes array */
 volatile struct rope *stakes[NROPES];
 
 static
@@ -38,15 +38,16 @@ bool initialize_data(){
 		rp->rp_number = i;
 		rp->rp_cut = false;
 
-		ropes[i] = *rp;
-		stakes[i] = &ropes[i];
+		ropes[i] = rp;
+		stakes[i] = ropes[i];
 	}
 
 	return true;
 }
 
 /* Synchronization primitives */
-struct lock*active_thread_lk;
+static struct lock *active_thread_lk; // lock for locking the active_threads variable
+static struct lock *ropes_lk; // lock for accessing the ropes
 
 /*
  * Describe your design and any invariants or locking protocols
@@ -60,9 +61,9 @@ dandelion(void *p, unsigned long arg)
 {
 
 	(void)arg;
+	(void)p;
 
 	int index;
-	struct lock *ropes_lk = p;
 
 	kprintf("Dandelion thread starting\n");
 
@@ -70,11 +71,10 @@ dandelion(void *p, unsigned long arg)
 		index = random() % NROPES;
 
 		lock_acquire(ropes_lk);
-		if (!ropes[index].rp_cut) {
-			ropes[index].rp_cut = true;
-			ropes_left--;
-
+		if (!ropes[index]->rp_cut) {
+			ropes[index]->rp_cut = true;
 			kprintf("Dandelion severed rope %d\n", index);
+			ropes_left--;
 		}
 		lock_release(ropes_lk);
 	}
@@ -93,7 +93,6 @@ marigold(void *p, unsigned long arg)
 	(void)arg;
 	(void)p;
 
-	struct lock *ropes_lk = p;
 	int index;
 
 	kprintf("Marigold thread starting\n");
@@ -104,9 +103,8 @@ marigold(void *p, unsigned long arg)
 		lock_acquire(ropes_lk);
 		if (!stakes[index]->rp_cut) {
 			stakes[index]->rp_cut = true;
-			ropes_left--;
-
 			kprintf("Marigold severed rope %d from stake %d\n", stakes[index]->rp_number,index);
+			ropes_left--;
 		}
 		lock_release(ropes_lk);
 	}
@@ -133,14 +131,13 @@ flowerkiller(void *p, unsigned long arg)
 	(void)p;
 	(void)arg;
 
-	struct lock *ropes_lk = p;
 	// the index of the stakes that will be swapped
 	int sk_index_1, sk_index_2;
 
 	kprintf("Lord FlowerKiller thread starting\n");
 
+get_random_index:
 	while(ropes_left > 1){
-	get_random_index:
 		sk_index_1 = random() % NROPES;
 		sk_index_2 = random() % NROPES;
 
@@ -199,26 +196,24 @@ airballoon(int nargs, char **args)
 
 	int err = 0, i;
 
-	struct lock *ropes_lk;
-
 	ropes_lk = lock_create("ropes-array-lock");
 	active_thread_lk = lock_create("active-thread-lock");
 
 	while(!initialize_data());
 
 	err = thread_fork("Marigold Thread",
-			  NULL, marigold, (struct lock *)ropes_lk, 0);
+			  NULL, marigold, NULL, 0);
 	if(err)
 		goto panic;
 
 	err = thread_fork("Dandelion Thread",
-			  NULL, dandelion, (struct lock *)ropes_lk, 0);
+			  NULL, dandelion, NULL, 0);
 	if(err)
 		goto panic;
 
 	for (i = 0; i < N_LORD_FLOWERKILLER; i++) {
 		err = thread_fork("Lord FlowerKiller Thread",
-				  NULL, flowerkiller, (struct lock *)ropes_lk, 0);
+				  NULL, flowerkiller, NULL, 0);
 		if(err)
 			goto panic;
 	}
@@ -235,11 +230,23 @@ panic:
 
 done:
 	while(active_threads > 0);
-	kprintf("WE ARE DONE\n");
+	kprintf("I'm stuck here\n");
+	lock_destroy(ropes_lk);
+	lock_destroy(active_thread_lk);
+
+	/* free up rope variables */
+	for (int i = 0; i < NROPES; i++) {
+		KASSERT(ropes[i] != NULL);
+		kfree(ropes[i]);
+	}
+
+
+	kprintf("Main thread done\n");
 	return 0;
 }
  /* 
- bmake does not work properly
+ move ropes lock to the top
+stakes has to be volatile
  lock shared counter
  more fine grained lock??
  clear out the kernel and variables

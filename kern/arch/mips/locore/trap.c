4361c6ca2d8cbab4ad4f68523f0f3c7c915b0781
Mojan Jamalzadeh <mojanjamalzadeh@yahoo.com>
2020-11-19 17:34:04 -0800

    fix kill_curthread

--- kern/arch/mips/locore/trap.c
+++ kern/arch/mips/locore/trap.c
@@ -39,6 +39,7 @@
 #include <vm.h>
 #include <mainbus.h>
 #include <syscall.h>
+#include <kern/wait.h>
 
 
 /* in exception-*.S */
@@ -111,6 +112,7 @@ kill_curthread(vaddr_t epc, unsigned code, vaddr_t vaddr)
 	/*
 	 * You will probably want to change this.
 	 */
+	sys__exit(_MKWAIT_SIG(sig));
 
 	kprintf("Fatal user mode trap %u sig %d (%s, epc 0x%x, vaddr 0x%x)\n",
 		code, sig, trapcodenames[code], epc, vaddr);
